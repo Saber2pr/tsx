a0bc9ffbedff9fa06271e3c80396fed6ed61d86c
saber2pr <saber2pr@gmail.com>
2019-06-18 17:25:48 +0000

    feat(core/utils/index.ts): export cleanObj, store

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,5 +1,7 @@
+export * from './clean-obj'
 export * from './micro-task'
 export * from './override'
+export * from './store'
 export * from './to-array'
 export * from './to-lowerCase'
 export * from './veryify-key'
