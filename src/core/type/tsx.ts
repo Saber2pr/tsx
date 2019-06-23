5b54b798b6ac113d827fb6444e1eae74569c1406
saber2pr <saber2pr@gmail.com>
2019-06-23 14:46:27 +0000

    fix(tsx.ts): ts 3.5, has update it

--- src/core/type/tsx.ts
+++ src/core/type/tsx.ts
@@ -5,13 +5,13 @@
  * @Last Modified time: 2019-06-16 21:36:17
  */
 import { VNode } from './v-node'
-import { HTML5Shim } from './html5-shim'
 import { TSX } from './attributes'
 
 declare global {
   namespace JSX {
-    type IntrinsicElements = HTML5Shim &
-      { [K in keyof HTMLElementTagNameMap]: TSX.IntrinsicAttributes<K> }
+    type IntrinsicElements = {
+      [K in keyof HTMLElementTagNameMap]: TSX.IntrinsicAttributes<K>
+    }
 
     interface Element extends VNode {}
 
